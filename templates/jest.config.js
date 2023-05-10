7061ee993fb5564a04a32c9c0d13ab046a49c86b
Finn Carstensen <39498668+Caramba997@users.noreply.github.com>
2023-05-10 20:40:10 +0200

    Repo classification V
    - All JS projects classified (79%)
    - Improvements
    - Jest config boilerplate to collect coverage

---
+++ templates/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  coverageDirectory: './',
+  collectCoverage: true,
+  // collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  // coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageReporters: ['json-summary'],
+  coverageThreshold: null
+};
